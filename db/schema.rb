6ee6bd20ec1da2ea4a840364d5fa04bd27016c0b
Daniel Sparano <dsparano1@gmail.com>
2013-03-12 00:41:17 -0400

    AddUserModelsWithDevise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130309074709) do
+ActiveRecord::Schema.define(:version => 20130312043718) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130309074709) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
